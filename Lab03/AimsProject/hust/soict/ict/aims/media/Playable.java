73936e34036a1cae39f0f3db053f97c7c4790a02
unknown <vm.dung.learning@gmail.com>
2025-04-20 05:56:45 +0700

    Lab 03 completed

---
+++ Lab03/AimsProject/hust/soict/ict/aims/media/Playable.java
@@ -0,0 +1,7 @@
+package hust.soict.ict.aims.media;
+
+public interface Playable {
+    
+    public void play();
+
+}
---
+++ Lab03/AimsProject/hust/soict/ict/aims/media/Track.java
@@ -0,0 +1,38 @@
+package hust.soict.ict.aims.media;
+
+public class Track {
+	private String title;
+    private int length;
+
+    // Constructor 
+    public Track(String title, int length) {
+        this.title = title;
+        this.length = length;
+    }
+    
+    // Getter method
+    public String getTitle() {
+        return title;
+    }
+    public int getLength() {
+        return length;
+    }
+    
+    // Play method
+    public void play() {
+        System.out.println("Playing track: " + this.getTitle());
+        System.out.println("Track length: " + this.getLength());
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {  
+            return true;
+        }
+        if (!(obj instanceof Track)) {
+            return false;
+        }
+        return ((Track)obj).getTitle() == this.getTitle() && ((Track)obj).getLength() == this.getLength();
+    }
+
+}
